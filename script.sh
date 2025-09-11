a8a822ca23bfd0361a139c0b9b294617da3ce884
tejas <tejgurav123@gmail.com>
2025-09-11 16:54:51 +0530

    script file changed locally

--- script.sh
+++ script.sh
@@ -2,8 +2,14 @@
 
 read -p "Enter a number " num
 
-for((i=o; i<=num; i++))
-do
-fact=(($fact*$i))
-done
-echo "Factorial is = {$fact}"
+if [ $num -le 0 ]
+then
+	echo "Wrong input"
+else
+	fact=1
+	for((i=1; i<=num; i++))
+	do
+		fact=$(($fact*$i))
+	done
+		echo "Factorial is = $fact"
+fi
